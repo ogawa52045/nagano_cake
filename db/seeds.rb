6ae60b0ace6f0fce7cd85b8ae5555970ae742e10
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-09-26 16:18:01 +0000

    signup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: "garyooon42@gmail.com",
+  password: "ogaryo19970402")
