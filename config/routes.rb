da186eea618f88be7cacb14611b5b408597b10a4
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-09-29 16:45:34 +0000

    [FINISH]sign_up and sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
     sessions: "public/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  get 'about' => 'homes#about'
+  
 end
