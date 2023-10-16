bda3b3ee25e50cc9f70f07f8b7d9e39859b61d1b
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-10-16 15:24:33 +0000

    [finish]cart_item_index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,27 @@
 class Public::CartItemsController < ApplicationController
-  def new
-    @cart_items = Cart_item.new
+  before_action :authenticate_customer!
+  def index
+    @cart_items = current_customer.cart_items.all
   end
   
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      render 'index'
+    else
+      render 'index'
+    end
+  end
+ 
   private
-  
+ 
   def cart_item_params
-    prams.require(:cart_item).permit(:item_id, :amount)
+    params.require(:cart_item).permit(:item_id, :price, :amount)
   end
 end
